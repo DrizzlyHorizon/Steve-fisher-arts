da28d024ab3b4ae7d1e9de0651f487ee5e3f453e
Devin <dvanste@gmail.com>
2024-10-07 11:03:31 -0500

    added more placeholder text

--- fisher/src/areas/Paintings/Paintings.tsx
+++ fisher/src/areas/Paintings/Paintings.tsx
@@ -10,5 +10,8 @@ export default function Paintings() {
   const categories = ['Paintings', 'Sculptures', 'Photography'];
   const artTypes = [paintings, sculptures, photography];
 
-  return <CategoryArtDisplay categories={categories} artTypes={artTypes} quote="Art is the journey of a free soul." />;
+  return <CategoryArtDisplay categories={categories} artTypes={artTypes} quote="Vestibulum a scelerisque ipsum, sit amet gravida turpis. Aenean velit massa, faucibus convallis lectus id, viverra tincidunt libero. 
+        Praesent id nisl ac ipsum bibendum sollicitudin eget non magna. Nulla sollicitudin mauris ac leo auctor iaculis. Cras sed felis nec felis suscipit
+        gravida sed id orci. Maecenas felis ante, pharetra et luctus ac, rhoncus sit amet lacus. Sed sit amet fermentum lacus. Integer condimentum cursus 
+        luctus." />;
 }
